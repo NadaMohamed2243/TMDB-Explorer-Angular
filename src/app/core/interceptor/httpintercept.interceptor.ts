7de0656471f9668dd177d8ca7605ca02396ac8ba
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-04-27 16:12:44 +0300

    Add HttpInterceptor to handel the header with the API Key

--- src/app/core/interceptor/httpintercept.interceptor.ts
+++ src/app/core/interceptor/httpintercept.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const httpinterceptInterceptor: HttpInterceptorFn = (req, next) => {
+  // HttpInterceptor
   const apiKey =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTZjYTY3ZDQ0ODU1NjBiMGYyNzgzYTMxOWM0NWQ5YiIsIm5iZiI6MTc0NTYxOTg1OS4zOTgwMDAyLCJzdWIiOiI2ODBjMGI5M2U1YzEwNWM4YTU2ZTE3NTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.lpLE2nhriZD6YPOFM_gB5sTO9mqZb6MzknxjBAG09xc';
   req = req.clone({
